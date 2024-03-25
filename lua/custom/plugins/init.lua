8a93267e9dfa06bbbc779332ed4e75d845acb6e0
Radhika Isswandhana <rads@rissw.tailcdbe.ts.net>
2024-03-25 12:48:51 +0700

    initial config from laptop

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,35 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      window = {
+        width = 0.7,
+      },
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>zm', ':ZenMode<cr>', { desc = '[Z]en [M]ode' })
+    end,
+  },
+  -- {
+  --   'Exafunction/codeium.vim',
+  --   event = 'BufEnter',
+  -- },
+}
