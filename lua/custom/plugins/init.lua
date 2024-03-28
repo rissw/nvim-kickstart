76ffdb553d6fc12e7b8c375698698a1d07031eec
rissw <radhika.iss@gmail.com>
2024-03-28 11:55:00 +0700

    add neogit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -33,4 +33,16 @@ return {
   --   'Exafunction/codeium.vim',
   --   event = 'BufEnter',
   -- },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true,
+  },
 }
