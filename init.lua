9ac624df39cb04e94057f275797bc607169c9879
rissw <radhika.iss@gmail.com>
2024-05-14 19:40:15 +0700

    add border

--- init.lua
+++ init.lua
@@ -760,6 +760,10 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
         },
+        window = {
+          completion = { border = 'rounded' },
+          documentation = { border = 'rounded' },
+        },
       }
     end,
   },
